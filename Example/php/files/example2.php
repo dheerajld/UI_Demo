31ba3f98c456817f98a3f18c11d31c6b292aa517
Dhiraj Dhas <dhiraj.dhas@coreviewsystems.com>
2023-02-17 10:59:06 +0530

    file in php

---
+++ Example/php/files/example2.php
@@ -0,0 +1,104 @@
+<?php
+$data = '{
+    "Afghanistan": "+93",
+    "Albania": "+355",
+    "Algeria": "+213",
+    "American Samoa": "+1-684",
+    "Andorra": "+376",
+    "Angola": "+244",
+    "Anguilla": "+1-264",
+    "Antarctica": null,
+    "Antigua and Barbuda": "+1-268",
+    "Argentina": "+54",
+    "Armenia": "+374",
+    "Aruba": "+297",
+    "Australia": "+61",
+    "Austria": "+43",
+    "Azerbaijan": "+994",
+    "Bahamas": "+1-242",
+    "Bahrain": "+973",
+    "Bangladesh": "+880",
+    "Barbados": "+1-246",
+    "Belarus": "+375",
+    "Belgium": "+32",
+    "Belize": "+501",
+    "Benin": "+229",
+    "Bermuda": "+1-441",
+    "Bhutan": "+975",
+    "Bolivia": "+591",
+    "Bosnia and Herzegovina": "+387",
+    "Botswana": "+267",
+    "Brazil": "+55",
+    "British Indian Ocean Territory": null,
+    "British Virgin Islands": "+1-284",
+    "Brunei": "+673",
+    "Bulgaria": "+359",
+    "Burkina Faso": "+226",
+    "Burundi": "+257",
+    "Cambodia": "+855",
+    "Cameroon": "+237",
+    "Canada": "+1",
+    "Cape Verde": "+238",
+    "Cayman Islands": "+1-345",
+    "Central African Republic": "+236",
+    "Chad": "+235",
+    "Chile": "+56",
+    "China": "+86",
+    "Christmas Island": "+61",
+    "Cocos Islands": "+61",
+    "Colombia": "+57",
+    "Comoros": "+269",
+    "Cook Islands": "+682",
+    "Costa Rica": "+506",
+    "Croatia": "+385",
+    "Cuba": "+53",
+    "Curacao": "+599",
+    "Cyprus": "+357",
+    "Czech Republic": "+420",
+    "Democratic Republic of the Congo": "+243",
+    "Denmark": "+45",
+    "Djibouti": "+253",
+    "Dominica": "+1-767",
+    "Dominican Republic": "+1-809, +1-829, +1-849",
+    "East Timor": "+670",
+    "Ecuador": "+593",
+    "Egypt": "+20",
+    "El Salvador": "+503",
+    "Equatorial Guinea": "+240",
+    "Eritrea": "+291",
+    "Estonia": "+372",
+    "Ethiopia": "+251",
+    "Falkland Islands": "+500",
+    "Faroe Islands": "+298",
+    "Fiji": "+679",
+    "Finland": "+358",
+    "France": "+33",
+    "French Polynesia": "+689",
+    "Gabon": "+241",
+    "Gambia": "+220",
+    "Georgia": "+995",
+    "Germany": "+49",
+    "Ghana": "+233",
+    "Gibraltar": "+350",
+    "Greece": "+30",
+    "Greenland": "+299",
+    "Grenada": "+1-473",
+    "Guam": "+1-671"
+}';
+
+
+$json = json_decode($data, true);
+
+$file = fopen("./country_phone.csv", "w");
+fputcsv($file, ['country', 'phone']);
+foreach ($json as $country => $phone) {
+    if ($country == "Ghana") {
+        echo "I want to $country I would like $phone";
+    }
+
+    fputcsv($file, [$country, $phone]);
+}
+
+fclose($file);
+
+// print_r($json);
